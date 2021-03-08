90ac83ffd0b8579d138bb98c8c4f81f7e60efaea
Claudio Barsante <clbmribas@gmail.com>
2021-03-08 20:38:11 -0300

    ajuste import shade

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,5 +1,5 @@
 import styled, { keyframes } from 'styled-components';
-import { shade } from 'polished';
+//import { shade } from 'polished';
 /**/
 import signInRightSideImage from '../../assets/sign-in.png';
 
